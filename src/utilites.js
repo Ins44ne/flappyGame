92a25be76164f7da5f97008f70ed9b0b8000193f
Ins44ne <ins44ne91@gmail.com>
2024-08-29 10:21:04 +0300

    faet: add Ranks

--- src/utilites.js
+++ src/utilites.js
@@ -5,3 +5,28 @@ export function makeBackgr(kont) {
     kont.fixed(),
   ])
 }
+
+export function computeRank(score) {
+  if (score > 30) {
+    return 'S'
+  }
+
+  if (score > 20) {
+    return 'A'
+  }
+
+  if (score > 10) {
+    return 'B'
+  }
+
+  if (score > 2) {
+    return 'C'
+  }
+
+  return 'D'
+}
+
+export function startGame(kont) {
+  kont.play('confirm')
+  kont.go('main')
+}
